7ac881b0a6f77d75dbfc35fd75c300bf262282f6
danrpinho <up201505302@fe.up.pt>
2017-05-02 14:24:16 +0100

    Added argument number and time unit verification

--- gerador.c
+++ gerador.c
@@ -1,12 +1,15 @@
 #include "gerador.h"
 
-
 int main(int argc, char *argv[]){
+    
     if (argc != 4){
         printf("Command usage: gerador <no. requests> <max time> <time unit>\n");
         exit(1);
     }
 
-    char* unidade = argv[3];
-
+    char *unidade = argv[3];
+    if (!(unidade[0] == 'u' || unidade[0] == 's' || unidade[0] == 'm')){
+        printf("Time unit usage: 's' for seconds, 'm' for milliseconds, 'u' for microseconds\n");
+        exit(2);
+    }
 }
--- sauna.c
+++ sauna.c
@@ -1,5 +1,18 @@
 #include "sauna.h"
 
+int main(int argc, char *argv[]){
+    
+    if (argc != 3){
+        printf("Command usage: gerador <no. seats> <time unit>\n");
+        exit(1);
+    }
 
+    char* unidade = argv[2];
+    if (!(unidade[0] == 'u' || unidade[0] == 's' || unidade[0] == 'm')){
+        printf("Time unit usage: 's' for seconds, 'm' for milliseconds, 'u' for microseconds\n");
+        exit(2);
+    }
+
+}
 
 
